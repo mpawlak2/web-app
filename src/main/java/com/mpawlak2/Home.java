27e01342139e35b37bf053d48fb7d56022694fc5
Mateusz Pawlak <mpawlak62@gmail.com>
2016-06-19 15:15:27 +0200

    minor controller update

--- src/main/java/com/mpawlak2/Home.java
+++ src/main/java/com/mpawlak2/Home.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class Home {
     @GetMapping("/")
     public String home() {
-        return "hello";
+        return "home";
     }
 }
