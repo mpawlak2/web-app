2c808e742511364e4e4b49bcf8d9ddde662be075
Mateusz Pawlak <mpawlak62@gmail.com>
2016-06-19 13:10:30 +0200

    update ignore

---
+++ src/main/java/com/mpawlak2/Home.java
@@ -0,0 +1,12 @@
+package com.mpawlak2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+    @GetMapping("/")
+    public String home() {
+        return "hello";
+    }
+}
