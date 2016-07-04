bc0b1b114b357ce9a6730cbb0cd117dcc6be55fe
Mateusz Pawlak <mpawlak62@gmail.com>
2016-07-04 20:44:04 +0200

    getmapping -> request mapping

--- src/main/java/com/mpawlak2/Home.java
+++ src/main/java/com/mpawlak2/Home.java
@@ -1,11 +1,11 @@
 package com.mpawlak2;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Home {
-    @GetMapping("/")
+    @RequestMapping("/")
     public String home() {
         return "home";
     }
